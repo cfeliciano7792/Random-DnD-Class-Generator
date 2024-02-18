78d100f5e83d31b8b7079c4bd2fd7bbb3df37d0f
cfeliciano <cfeliciano7792@gmail.com>
2024-02-18 16:10:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+// List of D&D classes
+const dndClasses = [
+  "Barbarian",
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Fighter",
+  "Monk",
+  "Paladin",
+  "Ranger",
+  "Rogue",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+  "Artificer",
+  "Blood Hunter"
+];
+// const dndClasses = [
+//   'yose',
+//   'aced',
+//   'badl',
+//   'seki',
+//   'zion',
+//   'brca',
+//   'care',
+//   'grta',
+//   'havo',
+//   'jotr',
+//   'hosp',
+//   'pefo'
+// ];
+
+// Route to get a random D&D class
+// app.get('/random-dnd-class', (req, res) => {
+//   const randomIndex = Math.floor(Math.random() * dndClasses.length);
+//   const randomClass = dndClasses[randomIndex];
+//   res.json({ class: randomClass });
+// });
+
+app.get('/random-dnd-class', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * dndClasses.length);
+  const randomClass = dndClasses[randomIndex];
+  res.json(randomClass);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
